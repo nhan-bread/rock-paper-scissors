f525acffce6c662e08a1e98295fb33452b08a961
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-06-04 17:17:06 -0700

    Create function playRound to play a round
    Takes players' choices, updates winner score, and logs message

--- javascript.js
+++ javascript.js
@@ -23,3 +23,53 @@ console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+console.log(humanScore);
+console.log(computerScore);
+
+// function playRound(humanChoice,computerChoice) {
+//    if (((humanChoice, computerChoice) === ("rock","paper"))
+//    || ((humanChoice, computerChoice) === ("paper","scissors"))
+//    || ((humanChoice, computerChoice) === ("scissors","rock"))) {
+//        computerScore++;
+//        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+//    }
+//    else if (((humanChoice, computerChoice) === ("rock", "scissors"))
+//         || ((humanChoice, computerChoice) === ("paper","rock"))
+//         || ((humanChoice, computerChoice) === ("scissors","paper"))) {
+//        humanScore++; 
+//        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+//    }
+//    else if (humanChoice === computerChoice) {
+//        console.log("It's a tie!")
+//    }
+//}
+
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "paper")
+    || (humanChoice === "paper" && computerChoice === "scissors")
+    || (humanChoice === "scissors" && computerChoice === "rock")) {
+        computerScore++;
+        //console.log(computerChoice)
+        //console.log(humanScore);
+        //console.log(computerScore);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+    else if ((humanChoice === "rock" && computerChoice === "scissors")
+    || (humanChoice === "paper" && computerChoice === "rock")
+    || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        //console.log(computerChoice);
+        //console.log(humanScore);
+        //console.log(computerScore);
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    }
+    else if (humanChoice === computerChoice) {
+        //console.log(computerChoice);
+        console.log("It's a tie!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
